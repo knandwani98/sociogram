6259a6c5feb8d20a81fe714d678ca437ddd3a794
Kushal Nandwani <kushalnandwani98@gmail.com>
2024-01-25 01:03:59 +0530

    Revert "navdata"
    This reverts commit f548a2319e3f0e9aac3b305681dda7d351055951.

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,26 +1,22 @@
 import React from "react";
-import * as data from "../../data/navLink.json";
 
 type Props = {};
 
 export const Sidebar = (props: Props) => {
   return (
-    <aside className="sidebar p-4">
-      <nav>
-        <a className="text-2xl font-semibold text-pink-500" href="/">
-          {process.env.BRAND}
-        </a>
-        <ul>
-          {data.sidebar.map((link) => {
-            return (
-              <li className="font-regular py-4" key={link.label}>
-                <span>{link.icon}</span>
-                <span>{link.label}</span>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
+    <aside className="sidebar">
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Search</li>
+                <li>Explore</li>
+                <li>Reels</li>
+                <li>Messages</li>
+                <li>Notifications</li>
+                <li>Create</li>
+                <li>Profile</li>
+            </ul>
+        </nav>
     </aside>
   );
 };
